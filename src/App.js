0fbf85d20004729b09c98de926e58dda0fc2f849
unknown <viktoriadoganci@gmail.com>
2023-02-28 09:48:25 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {data} from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[doing,SetDoing] = useState(data);
+  const[showText,setShowText]=useState(false);
+  const removeTask = (id) => {
+    let newTasks= doing.filter((doin) => doin.id !== id);
+    SetDoing(newTasks)
+  }
+  const showTextClick = (item) => {
+    item.showMore=!item.showMore
+    setShowText(!showText)
+  }
+  return(
+    <div>
+    <div className='container'>
+      <h1>{doing.length} - things left to do for my happiness today</h1>
     </div>
-  );
+    {doing.map((doin => {
+      const{ id, task, text, image,showMore} = doin;
+      return(
+        <div key={id}>
+        <div className='container'>
+          <h2>{id}.{task}</h2>
+           </div>
+           <div className='container'>
+          <img src={image} width="500px"/>
+           </div>
+
+           <div className='container'>
+          <p>{showMore?text : text.substring(0,96)+"..."}
+          <button onClick={() => showTextClick(doin)}>{showMore ? "Show less":"Show more"}</button></p>
+           </div>
+           <div className='container'>
+          <button className="btn" onClick={() => removeTask(id)}>Done</button>
+           </div>
+           </div>
+           
+      )
+    }))}
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+    {
+        id:1,
+        task:"Hugs of loved ones",
+        text:"It's a deeper, more intimate level of connection that you simply don't get from talking. The second reason is that physical touch causes the release of the “love hormone” oxytocin, which improves bonding and trust and makes people feel closer to one another.",
+        image:"https://t4.ftcdn.net/jpg/03/18/73/27/240_F_318732776_XPB2WP21bEFL1eZHgVRa2eCkRHJD0M3N.jpg",
+        showMore:false
+    },
+    {
+        id:2,
+        task:"Сommunication with friends",
+        text:"Communication is important in any relationship, as it allows you to effectively share feelings, opinions and expectations. Many people fail to communicate due to a fear of rejection or the fear that they'll end up upsetting or even losing their partner, family members or friends.",
+        image:"https://img.freepik.com/free-photo/girls-cafe_1157-5249.jpg?w=826&t=st=1676482159~exp=1676482759~hmac=935cadabb175b39638b487d2734bef6b7af6b3bb5d5b45992a521af5f5f0c5f5",
+        showMore:false
+    },
+    {
+        id:3,
+        task:"To do smth new",
+        text:"One of the most crucial reasons to learn something new is that you gain power when you do. This new-found power means you can don't need someone else to fix, mend or make something in your home or that you own. You are empowered to do it yourself!",
+        image:"https://img.freepik.com/free-photo/girl-rides-horse_1321-1243.jpg?w=826&t=st=1676482237~exp=1676482837~hmac=12fbd09edef3f8634d706980d3b25aaebece3194d8610f279b6d4a49357cae57",
+        showMore:false
+
+    },
+    {
+        id:4,
+        task:"Dancing",
+        text:"Dancing promotes confidence, self esteem, personal expression and flexibility of the body and mind. Dance classes promote physical fitness as being fun and easy and are the catalyst to engage in fitness for a lifetime.",
+        image:"https://img.freepik.com/free-photo/modern-dancer-jumping_1163-3789.jpg?w=826&t=st=1676482367~exp=1676482967~hmac=ad7e94a6c0bde3d5b5f752ed1e92678d23c4dbfd4008e0e793204289263849de",
+        showMore:false
+    },
+    {
+        id:5,
+        task:"Play chess",
+        text:"Chess is about making strong moves and sticking by them. In life too, if we plan or do something, we need to make sure to stand on our ground and fight for what we think is better for us. Be aggressive and take action When there is a problem or a threat in chess, we sometimes need to be aggressive and take action.",
+        image:"https://img.freepik.com/free-photo/business-strategy-ideas-concept-hand-play-chess-board_609648-1912.jpg?w=826&t=st=1676482522~exp=1676483122~hmac=ce18ac008a805a830feece00476146eb1da8aa06276d62baa421b8194e362a93",
+        showMore:false
+    },
+    {
+        id:6,
+        task:"Listen to music",
+        text:"Research has shown that listening to music can reduce anxiety, blood pressure, and pain as well as improve sleep quality, mood, mental alertness, and memory.",
+        image:"https://img.freepik.com/free-photo/carefree-fashionable-asian-woman-smiles-positively-keeps-eyes-closed-hand-face-has-fun-listens-music-holds-modern-smartphone-wears-wireless-headphones-expresses-positive-emotions-poses-indoor_273609-49594.jpg?w=826&t=st=1676482579~exp=1676483179~hmac=0ae7e3c12b8dd84fc5d9f0571a1dfd486915e90e936def591d973e5e5555f96f",
+        showMore:false
+    },
+]
